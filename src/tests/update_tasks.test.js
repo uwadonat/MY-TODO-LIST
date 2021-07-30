412e9c3aab1ce792ec1a08d23aa7c2c28fab6132
uwadonat <uwadonat@gmail.com>
2021-07-30 11:46:30 +0200

    Test method to remove element from todo list

--- src/tests/update_tasks.test.js
+++ src/tests/update_tasks.test.js
@@ -8,7 +8,7 @@ jest.mock("./test_files/add_remove");
 displayTasks();
 
 describe("test adding and removing task method ", () => {
-    test("add one li element to the ul", () => {
+    test("add one li element to the ul:", () => {
         const input = global.document.getElementById("description");
         input.value = "Washing my body";
         const task = { id: 3, index: 1, description: "Washing my body", completed: true };
@@ -22,5 +22,16 @@ describe("test adding and removing task method ", () => {
     
           expect(lenA).toBe(lenB + 1);  
         })
-    })
+    });
+
+    test("remove the li element from the ul:", () => {
+        const listBefore = global.document.getElementsByTagName("li");
+        const lenB = listBefore.length;
+        removeTask("div2", tasks);
+        setTimeout(500, () => {
+          const listAfter = global.document.getElementsByTagName("li");
+          const lenA = listAfter.length;
+          expect(lenB - 1).toBe(lenA);
+        }); 
+    });
 });
